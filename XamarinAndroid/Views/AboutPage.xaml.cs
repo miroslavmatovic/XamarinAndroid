24f34f04ae5473d6138dab11ba20a111d3991cf0
miroslavmatovic <v-mirosm@microsoft.com>
2021-02-24 13:53:54 +0100

    initial

---
+++ XamarinAndroid/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinAndroid.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XamarinAndroid/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinAndroid.ViewModels;
+
+namespace XamarinAndroid.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
